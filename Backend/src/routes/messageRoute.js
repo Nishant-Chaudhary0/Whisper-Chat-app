6dadd3e6dd72a5d32864d26fcdab13fbe497cdaf
Nishant Chaudhary <nishantchaudhary7557@gmail.com>
2025-04-26 21:00:54 +0530

    Route changes

--- Backend/src/routes/messageRoute.js
+++ Backend/src/routes/messageRoute.js
@@ -5,8 +5,8 @@ import { getMessages, getUsersForSidebar, sendMessage } from '../controllers/mes
 
 const router = express.Router();
 
-router.get("/users",protectRoute, getUsersForSidebar)
-router.get("/:id",protectRoute, getMessages)
-router.post("/send/:id", protectRoute, sendMessage)
+router.get("/users", protectRoute, getUsersForSidebar); // Static route
+router.get("/:id", protectRoute, getMessages);         // Dynamic route
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
